3ee5d5c658c5e699e4f127b97c4c27cc76275fb3
ToniSHernandez <toni@kerigan.com>
2021-07-28 08:07:46 -0500

    Added checkbox.Not fully functional.

--- wp-content/themes/kma-slim/template-parts/forms/consult-form.php
+++ wp-content/themes/kma-slim/template-parts/forms/consult-form.php
@@ -16,6 +16,12 @@
             <div class="column is-12">
                 <input type="email" name="email_address" class="input email" placeholder="Email Address" required>
             </div>
+            <div class="custom-control custom-checkbox" style="margin: .5em 0 .5em 1em;">
+                <input id="terms-checkbox" type="checkbox" value="Terms Accepted" class="custom-control-input">
+                <label for="terms-checkbox" class="tiny-text custom-control-label" >
+                    By checking this box, I agree that I have read the <a href="/terms-of-service/">Terms of Service</a> and <a href="/third-party-advertisements/">Third-Party Advertisements</a> agreements and that I accept the provisions of said agreements. If unwilling to consent, call <a href="tel:9293796953" style="text-decoration: underline;">929-379-6953</a> to learn more about our services. Other terms and conditions may apply.
+                </label>
+            </div>
             <div class="column is-12">
                 <button class="button is-primary" type="submit">submit</button>
             </div>
